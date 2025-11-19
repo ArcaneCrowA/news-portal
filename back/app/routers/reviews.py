960ee1862632408937bd06de871ace7406f9637f
Adilet <adiletbeishekeyev@gmail.com>
2025-11-19 18:57:30 +0500

    working bookreview

--- back/app/routers/reviews.py
+++ back/app/routers/reviews.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from sqlmodel import Session
+from sqlmodel import Session, select
 
 from .. import crud, models
 from ..db import get_session
@@ -12,3 +12,10 @@ def create_review(
     review: models.Review, session: Session = Depends(get_session)
 ):
     return crud.create_review(session, review)
+
+
+@router.get("/")
+def list_reviews(book_id: int, session: Session = Depends(get_session)):
+    return session.exec(
+        select(models.Review).where(models.Review.book_id == book_id)
+    ).all()
